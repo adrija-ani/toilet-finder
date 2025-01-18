803747bda164ac163e6d9450b5db151e2f5938c3
adrija-ani <148892976+adrija-ani@users.noreply.github.com>
2025-01-18 19:07:49 +0530

    Delete OneDrive/Desktop/Projects/Flutter/blood-donation-app-flutter-main directory

--- OneDrive/Desktop/Projects/Flutter/blood-donation-app-flutter-main/mobile/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:blood_donation/main.dart';
-
-void main() {
-  testWidgets('App Works', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-  });
-}
